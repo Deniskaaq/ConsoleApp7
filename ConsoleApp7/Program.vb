de96e55d24b62af8df49bd395fa00a9240eb4485
Deniskaaq <158272790+Deniskaaq@users.noreply.github.com>
2024-06-12 23:29:00 +0200

    Update Program.vb

--- ConsoleApp7/Program.vb
+++ ConsoleApp7/Program.vb
@@ -1,22 +1,22 @@
 Module Module1
     Sub Main()
-        ' Zjistíme, kolik èísel chce uživatel zadat
-        Console.WriteLine("Zadejte poèet èísel, které chcete zadat:")
+        
+        Console.WriteLine("Zadejte poÃ¨et Ã¨Ã­sel, kterÃ© chcete zadat:")
         Dim pocetCisel As Integer = Convert.ToInt32(Console.ReadLine())
-        ' Pole pro uložení èísel
+        
         Dim cisla(pocetCisel - 1) As Integer
-        ' Cyklické zadávání èísel
+        ' 
         For i As Integer = 0 To pocetCisel - 1
-            Console.WriteLine("Zadejte èíslo " & (i + 1) & ":")
+            Console.WriteLine("Zadejte Ã¨Ã­slo " & (i + 1) & ":")
             cisla(i) = Convert.ToInt32(Console.ReadLine())
         Next
-        ' Vyhodnocení výsledkù
-        Console.WriteLine("Zadali jste následující èísla:")
+        
+        Console.WriteLine("Zadali jste nÃ¡sledujÃ­cÃ­ Ã¨Ã­sla:")
         For Each cislo As Integer In cisla
             Console.WriteLine(cislo)
         Next
-        ' Ukonèení programu
-        Console.WriteLine("Stisknìte libovolnou klávesu pro ukonèení programu.")
+        
+        Console.WriteLine("StisknÃ¬te libovolnou klÃ¡vesu pro ukonÃ¨enÃ­ programu.")
         Console.ReadKey()
     End Sub
 End Module
